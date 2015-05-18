738238fcb8c9804814fc63b800be8497288cb792
adriano teixeira <adriano.teixeira@pmonteiro.pt>
2015-05-18 18:41:53 +0100

    list groups

--- WebContent/app/controllers/ExerciseController.js
+++ WebContent/app/controllers/ExerciseController.js
@@ -3,10 +3,17 @@
 
 var exercise = angular.module('gymApp.exercise');
 
-exercise.controller('ExerciseController', ['$scope','$http',ExerciseController]);
+exercise.controller('ExerciseController', ['$scope','$http','globals',ExerciseController]);
 
-function ExerciseController($scope,$http) {
-	
+function ExerciseController($scope,$http,globals) {
+
+	$scope.groups = [];
+
+	$http.get( globals.url + 'groups' , {} ).then(function(response) {
+
+		$scope.groups = response.data;
+		console.log($scope.groups);
+	});	
 }
 
 })(window);
