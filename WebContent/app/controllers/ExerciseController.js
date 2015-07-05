498292c011d3cbceb1765c0326e88863c4cc3988
adriano <adrianojlt@gmail.com>
2015-07-05 23:58:20 +0100

    list of exercises

--- WebContent/app/controllers/ExerciseController.js
+++ WebContent/app/controllers/ExerciseController.js
@@ -4,37 +4,101 @@
 //var exercise = angular.module('gymApp.exercise',[]);
 var exercise = angular.module('gymApp.exercise',[]);
 
-exercise.controller('ExerciseController', ['$scope','$http','$routeParams','content','globals',ExerciseController]);
+exercise.controller('ExerciseController', ['$scope','$http','$routeParams','oneGroup','globals','MuscleGroups',ExerciseController]);
+exercise.directive('contentPage', ['$location',ContentPage]);
 
-function ExerciseController($scope,$http,$routeParams,content,globals) {
+exercise.factory('MuscleGroups',['$http','globals',function($http,globals) {
 
-	//console.log('ExerciseController');
+	self = this;
+
+	var dataFactory = {groups:{}};
+	var promise;
+	
+	dataFactory.groups.getEvent = function(event) {
+
+		if ( !promise ) {
+		 	promise = $http.get( globals.url + 'groups', event ).error(function() { // this is optional
+            	promise = false;
+           	});
+		}
+
+		return promise;
+	};
+
+	dataFactory.group = function(id) {
+		return $http.get( globals.url + 'groups/' + id , {} );
+	}
+
+	return dataFactory;
+}]);
+
+function ExerciseController($scope,$http,$routeParams,oneGroup,globals,MuscleGroups) {
+
+	var self = this;
 
 	$scope.groups = [];
+	$scope.group = {};
 	$scope.actions = {};
 
-	$http.get( globals.url + 'groups' , {} ).then(function(response) {
-		$scope.groups = response.data;
-	});	
+	// call groups only one time ...
+	MuscleGroups.groups.getEvent().success(function(data) {
+		$scope.groups = data;
+	}).error(function(error) { } );
+
+	if ( oneGroup ) {
+		MuscleGroups.group($routeParams.groupID).success(function(response) {
+			$scope.group = response;
+		})
+	}
 
 	$scope.actions.isActive = function(id) {
-		//console.log(id);
 		return parseInt($routeParams.groupID) === id;
 	};
 
-	$scope.click = function() {
-		console.log('click');
+	$scope.actions.changeGroup = function(id) {
+
+		return;
+
+		// this doesnt work ... $scope is not refreshed ...
+		$http.get( globals.url + 'groups/' + id , {} ).then( function(response) {
+			$scope.group = response.data;
+			$scope.$apply(function() {
+				console.log('apply');
+			});
+		}, function(err) { } );	
 	};
 
 	$scope.isGroupPage = function() {
 		return $routeParams.groupID;
 	};
 
-	if ( content ) {
+	$scope.test = function() {
 
-	}
-	else {
+	};
 
+	$scope.$watch('group',function(newVal,oldVal){
+		//console.log(newVal, oldVal);
+	});
+}
+
+function ContentPage() {
+
+	return {
+
+		restrict: 'AE',
+
+    	link: function($scope, element, attrs, ctrl) {
+			//console.log('link: contentPage');
+    	},
+
+		controller: function($scope) {
+
+			var self = this;
+
+			self.init = function(elem) {
+
+			};
+		}
 	}
 }
 
