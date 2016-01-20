e0f01b046bc8f37b7a412539aaa9063a5050caee
adriano <adrianojlt@gmail.com>
2016-01-20 09:24:43 +0000

    add to header content-range

--- src/pt/adrz/gymlogger/service/WorkoutService.java
+++ src/pt/adrz/gymlogger/service/WorkoutService.java
@@ -2,15 +2,17 @@ package pt.adrz.gymlogger.service;
 
 import java.util.List;
 
-import javax.ws.rs.ApplicationPath;
+import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.WebApplicationException;
-import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -18,11 +20,9 @@ import pt.adrz.gymlogger.dao.RepetitionDAO;
 import pt.adrz.gymlogger.dao.RepetitionDAOJDBC;
 import pt.adrz.gymlogger.dao.WorkoutDAO;
 import pt.adrz.gymlogger.dao.WorkoutDAOJDBC;
-import pt.adrz.gymlogger.model.Exercise;
 import pt.adrz.gymlogger.model.Repetition;
 import pt.adrz.gymlogger.model.Workout;
 
-//@ApplicationPath("rest")
 @Path("/workouts")
 public class WorkoutService {
 	
@@ -36,8 +36,36 @@ public class WorkoutService {
 
 	@GET
 	@Produces(MediaType.APPLICATION_JSON) 
-	public List<Workout> getWorkouts() {
-		return workoutsDAO.getWorkoutsWithRepetitions();
+	public List<Workout> getWorkouts(
+			@QueryParam("offset") Integer offset, @QueryParam("limit") Integer limit,
+			@Context HttpHeaders header, @Context HttpServletResponse response) {
+		
+		List<Workout> workouts = null;
+		StringBuilder contentLength = new StringBuilder();
+		
+		try {
+			
+			if ( offset == null || limit == null ) {
+				workouts = workoutsDAO.getWorkoutsWithRepetitions();
+			}
+			else {
+				
+				workouts = workoutsDAO.getWorkouts(offset, limit);
+	
+				contentLength.append("items ");
+				contentLength.append(offset);
+				contentLength.append("-");
+				contentLength.append((offset + workouts.size() - 1));
+				contentLength.append("/");
+				contentLength.append(workoutsDAO.getNoOfRecords());
+			}
+		}
+		catch (Exception e) {
+			throw new WebApplicationException(e.getMessage(), Response.Status.BAD_REQUEST);
+		}
+		
+		response.setHeader("Content-Range", contentLength.toString());
+		return workouts;
 	}
 	
 	@GET
